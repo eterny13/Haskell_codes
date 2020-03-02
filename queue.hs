a63fa2e9905f0adc94ed0192e7039d709611b093
Eiichiro <eiichi.kendoruki@gmail.com>
2020-03-02 14:28:03 +0900

    Update queue.hs

--- queue.hs
+++ queue.hs
@@ -18,5 +18,5 @@ queue' t q (x:xs) =
 
 main = do
   let ans = queue 0 100 [("p1", 150),("p2", 80),("p3", 200),("p4", 350),("p5", 20)] 
-  mapM_ print ans
-  
+  forM_ ans $ \(a,b) -> do
+    printf "%s %d\n" a b
